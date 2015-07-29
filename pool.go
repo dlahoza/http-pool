d1ce9809d446b472121fc82804eef975a91c2294
Lagoza Dmitriy Sergeevich <lagoza-ds@fxclub.org>
2015-07-29 11:02:43 +0200

    Code formatting

--- pool.go
+++ pool.go
@@ -4,20 +4,20 @@
 package http
 
 import (
-	"time"
 	"net"
+	"time"
 )
 
 type Pool struct {
-	total, busy uint
-	jobsChan chan *job
+	total, busy   uint
+	jobsChan      chan *job
 	startQuitChan chan bool
-	fullStopChan chan bool
-	busyFreeChan chan bool
+	fullStopChan  chan bool
+	busyFreeChan  chan bool
 }
 
 type job struct {
-	c *conn
+	c    *conn
 	stop bool
 }
 
@@ -26,24 +26,24 @@ type job struct {
 func (p *Pool) manager() {
 	for {
 		select {
-			case c:= <-p.startQuitChan:
-				if (c) {
-					go p.worker()
-					p.total++
-				} else {
-					p.total--
-				}
-			case b:= <-p.busyFreeChan:
-				if (b) {
-					p.busy++
-				} else {
-					p.busy--
-				}
-			case <- p.fullStopChan:
-				if p.total > 0 {
-					p.StopWorker()
-					p.fullStopChan <- true
-				}
+		case c := <-p.startQuitChan:
+			if c {
+				go p.worker()
+				p.total++
+			} else {
+				p.total--
+			}
+		case b := <-p.busyFreeChan:
+			if b {
+				p.busy++
+			} else {
+				p.busy--
+			}
+		case <-p.fullStopChan:
+			if p.total > 0 {
+				p.StopWorker()
+				p.fullStopChan <- true
+			}
 		}
 	}
 }
@@ -52,53 +52,54 @@ func (p *Pool) manager() {
 func (p *Pool) worker() {
 	for {
 		job := <-p.jobsChan
-		if (job.stop) {
-			p.startQuitChan<-false
+		if job.stop {
+			p.startQuitChan <- false
 			return
 		}
-		p.busyFreeChan<-true
+		p.busyFreeChan <- true
 		job.c.serve()
-		p.busyFreeChan<-false
+		p.busyFreeChan <- false
 	}
 }
 
 // GetTotal get total count of workers in the pool
-func (p *Pool) GetTotal() (uint) {
+func (p *Pool) GetTotal() uint {
 	return p.total
 }
 
 // GetFree get count of free workers in the pool
-func (p *Pool) GetFree() (uint) {
-	return p.total-p.busy
+func (p *Pool) GetFree() uint {
+	return p.total - p.busy
 }
 
 // GetBusy get count of busy workers in the pool
-func (p *Pool) GetBusy() (uint) {
+func (p *Pool) GetBusy() uint {
 	return p.busy
 }
 
 // AddJob adds connection to the job's channel
 func (p *Pool) AddJob(c *conn) {
-	p.jobsChan<-&job{c: c, stop: false}
+	p.jobsChan <- &job{c: c, stop: false}
 }
 
 // StopWorker sends stop signal to a single worker
 func (p *Pool) StopWorker() {
-	p.jobsChan<-&job{c: nil, stop: true}
+	p.jobsChan <- &job{c: nil, stop: true}
 }
 
 // StartWorker starts a single worker
 func (p *Pool) StartWorker() {
-	p.startQuitChan<-true
+	p.startQuitChan <- true
 }
 
 // StartWorkers starts a multiple workers
 func (p *Pool) StartWorkers(workerCount uint) {
 	var i uint
-	for i=0; i<workerCount; i++ {
+	for i = 0; i < workerCount; i++ {
 		p.StartWorker()
 	}
 }
+
 // FullStop sends the stop signal to all workers in pool
 func (p *Pool) FullStop() {
 	p.fullStopChan <- true
@@ -106,8 +107,8 @@ func (p *Pool) FullStop() {
 
 // NewPool creates new pool instance, starts the pool manager and workers.
 // For empty pool with pool manager only use workerCount with 0 value.
-func NewPool(workerCount uint) (*Pool){
-	pool:=&Pool{jobsChan: make(chan *job), startQuitChan: make(chan bool, 1), fullStopChan: make(chan bool, 1), busyFreeChan: make(chan bool)}
+func NewPool(workerCount uint) *Pool {
+	pool := &Pool{jobsChan: make(chan *job), startQuitChan: make(chan bool, 1), fullStopChan: make(chan bool, 1), busyFreeChan: make(chan bool)}
 	go pool.manager()
 	pool.StartWorkers(workerCount)
 	return pool
@@ -157,13 +158,13 @@ func (srv *Server) ListenAndServeWithPool(pool *Pool) error {
 //              }
 //      }
 func ListenAndServeWithPool(addr string, handler Handler, pool *Pool) error {
-        server := &Server{Addr: addr, Handler: handler}
-        return server.ListenAndServeWithPool(pool)
+	server := &Server{Addr: addr, Handler: handler}
+	return server.ListenAndServeWithPool(pool)
 }
 
 // ServeWithPool accepts incoming connections on the Listener l, sends a job to pool
 // for each.  The pool read requests and then call srv.Handler to reply to them.
-func (srv *Server) ServeWithPool(l net.Listener, pool *Pool)error {
+func (srv *Server) ServeWithPool(l net.Listener, pool *Pool) error {
 	defer l.Close()
 	var tempDelay time.Duration // how long to sleep on accept failure
 	for {
